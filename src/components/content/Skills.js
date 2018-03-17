a0e1a35293d55bf0b0107844abf3e9d41bf1e105
Kerry Gougeon Ducharme <kerrygougeon@gmail.com>
2018-03-17 15:35:18 -0400

    skills

--- src/components/content/Skills.js
+++ src/components/content/Skills.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import Java from './../../assets/java.png'
+import Python from './../../assets/python.png'
+import HTML from './../../assets/html.png'
+import CSS3 from './../../assets/css.png'
+import Javascript from './../../assets/js.png'
 
 const Skills  = () => (
     <div className="App-content">
-        <h2>Skills</h2>
-        <p>
-        </p>
+        <h2>Languages</h2> 
+        <div id="skill-logo-set">
+            <a href="https://www.java.com/" id="skill-logo">
+                <img src={Java} alt="Java"/>
+            </a>
+            <a href="https://www.python.org/" id="skill-logo">
+                <img src={Python} alt="Python"/>
+            </a>
+            <a href="https://www.w3.org/html/" id="skill-logo">
+                <img src={HTML} alt="HTML"/>
+            </a>
+            <a href="https://www.w3.org/Style/CSS/" id="skill-logo">
+                <img src={CSS3} alt="CSS"/>
+            </a>
+            <a href="https://www.javascript.com/" id="skill-logo">
+                <img src={Javascript} alt="Javascript"/>
+            </a>
+        </div>    
+        <h2>Tools</h2>
+        <div id="skill-logo-set">
+            <a href="" id="skill-logo"><img src="" alt="React"/></a>
+            <a href="" id="skill-logo"><img src="" alt="Redux"/></a>
+            <a href="" id="skill-logo"><img src="" alt="Travis"/></a>
+            <a href="" id="skill-logo"><img src="" alt="Firebase"/></a>
+            <a href="" id="skill-logo"><img src="" alt="Digital Ocean"/></a>
+            <a href="" id="skill-logo"><img src="" alt="Sass"/></a>
+            <a href="" id="skill-logo"><img src="" alt="Git"/></a>
+            <a href="" id="skill-logo"><img src="" alt="Flask"/></a>
+            <a href="" id="skill-logo"><img src="" alt=""/></a>
+        </div>
     </div>
 );
 
